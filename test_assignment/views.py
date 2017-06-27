dbf11e542b3addc1ae5fdde4eb1d507b3867536d
ibrahem3amer <ibrahem3amer@hotmail.com>
2017-06-27 11:59:03 +0200

    ticket:1 Model storing data for view.

--- test_assignment/views.py
+++ test_assignment/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.core.management import call_command
+from test_assignment.models import Person
 
 def homepage_visitor(request):
     """
     Accepts http request, return homepage for non-authorized user. 
     """
     hard_coded_data = call_command('loaddata', 'data.json', app_label='test_assignment')
-    return render(request, 'home.html')
+    person          = get_object_or_404(Person, pk = 1)
+    return render(request, 'home.html', {'person': person})
