e151671777b92ffd96a5007599863ddb580cfb5b
ibrahem3amer <ibrahem3amer@hotmail.com>
2017-07-04 18:42:16 +0200

    ticket:5 Image preview using fileAPI added.

--- test_assignment/static/js/main.js
+++ test_assignment/static/js/main.js
@@ -41,6 +41,28 @@ function create_post() {
     });
 };
 
+// Handling image preview.
+function handleFiles(files) {
+
+    preview.innerHTML = ''
+    for (var i = 0; i < files.length; i++) {
+        var file = files[i];
+        var imageType = /^image\//;
+
+        if (!imageType.test(file.type)) {
+          continue;
+        }
+
+        var img = document.createElement("img");
+        img.classList.add("obj");
+        img.file = file;
+        preview.appendChild(img); // Assuming that "preview" is the div output where the content will be displayed.
+
+        var reader = new FileReader();
+        reader.onload = (function(aImg) { return function(e) { aImg.src = e.target.result; }; })(img);
+        reader.readAsDataURL(file);
+    }
+}
 
 
 // CSRF Protection code.
